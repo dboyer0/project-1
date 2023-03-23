ec27f7fc692a36e0b24d10cdce128622b23acd95
dallas <dboyer@dexter.com>
2023-03-23 06:44:13 -0500

    Feature: laid base of book shelf and book search pages from starter

--- src/BookSearch.js
+++ src/BookSearch.js
@@ -2,8 +2,26 @@ import './App.css';
 
 function BookSearch() {
   return (
-    <div className="app">
-      This is the search bar page
+    <div className="search-books">
+        <div className="search-books-bar">
+            <a
+                className="close-search"
+                // onClick={() => setShowSearchpage(!showSearchPage)}
+            >
+                Close
+            </a>
+
+            <div className="search-books-input-wrapper">
+                <input
+                    type="text"
+                    placeholder="Search by title, author, or ISBN"
+                />
+            </div>
+        </div>
+
+        <div className="search-books-results">
+            <ol className="books-grid"></ol>
+        </div>
     </div>
   );
 }
