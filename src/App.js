0ca6f2e5382f3ffc45cba9575b2bd9d86dfff611
dallas <dboyer@dexter.com>
2023-03-25 09:37:50 -0500

    Feature: dynamically fetching books from remote API

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import './App.css';
+import { useEffect, useState } from "react";
 import { Route, Routes } from "react-router-dom";
+import * as BooksAPI from "./BooksAPI";
 import BookShelf from "./BookShelf";
 import BookSearch from "./BookSearch";
 
 const App = () => {
+
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    const getBooks = async () => {
+      const res = await BooksAPI.getAll();
+      setBooks(res);
+    };
+
+    getBooks();
+  }, []);
+
+  console.log("Books List: ", books);
+
   return (
     <div className="app">
       <Routes>
