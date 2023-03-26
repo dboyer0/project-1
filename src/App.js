1c3a068d7809c0f7f4a370a9c56ee8bec00776dd
dallas <dboyer@dexter.com>
2023-03-26 05:35:45 -0500

    Feature: update book shelf.  Untested.  TODO add to Book component to test and debug

--- src/App.js
+++ src/App.js
@@ -19,6 +19,28 @@ const App = () => {
     getBooks();
   }, []);
 
+  const onBookUpdate = (book, shelf) => {
+    // pass in book and new shelf
+    // create new book with updated shelf
+    // PUT /update(book,shelf)
+    // get response
+    // call setBooks to update book state
+    // concat book to array of books
+
+    BooksAPI.update(book, shelf).then(() => {
+      let newBook = book
+      console.log("newBook before updating state: ", newBook)
+      newBook.shelf = shelf
+      console.log("newBook with updated shelf: ", newBook)
+      // remove book from state
+      // add the updated book to state
+      // this.setState((prevState) => ({ books: prevState.books.filter(b => b.id !== book.id).concat(newBook) }))
+      setBooks((books) => ({ books: books.filter(b => b.id !== book.id).concat(newBook) }))
+      console.log("Books array after updating state: ", books)
+    })
+  }
+
+
   console.log("Books List: ", books);
 
   return (
@@ -32,7 +54,7 @@ const App = () => {
               currentlyReading={books.filter(book => book.shelf === "currentlyReading")}
               read={books.filter(book => book.shelf === "read")}
               wantToRead={books.filter(book => book.shelf === "wantToRead")}
-              onBookUpdate={()=>{}}
+              onBookUpdate={onBookUpdate}
             />
           }
         />
